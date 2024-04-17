ac0b2efe2b7f9d9b166905b8dff3c0d041f98e24
Zhiqiang Liu <136632428+Lza12a@users.noreply.github.com>
2024-04-17 11:40:19 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-conda create -n hahe -y python=3.8
-conda activate hahe
+conda create -n unihr -y python=3.8
+conda activate unihr
 
 conda install scikit-learn -y
 conda install numpy -y
@@ -9,7 +9,4 @@ conda install pandas -y
 conda install tqdm -y
 
 pip install torch-1.10.0+cu113-cp38-cp38-linux_x86_64.whl
-pip install torch_cluster-1.6.0-cp38-cp38-linux_x86_64.whl
-pip install torch_scatter-2.0.9-cp38-cp38-linux_x86_64.whl
-pip install torch_sparse-0.6.13-cp38-cp38-linux_x86_64.whl
-pip install -U torch_geometric
+pip install dgl-cu116 dglgo -f https://data.dgl.ai/wheels/repo.html
